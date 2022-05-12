6a6dcd42bb13973dd0e41db3bd1664581fb5429e
alejandrosantanabonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-05-12 11:35:32 +0100

    Update test_form_print.py
    changed the test for printing

--- tests/test_form_print.py
+++ tests/test_form_print.py
@@ -1,19 +1,24 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+from pysoftk.linear_polymer.super_monomer import *
 from pysoftk.format_printers.format_mol import *
 
 # Molecules 1 and 2
 mol_1=Chem.MolFromSmiles('c1(ccc(cc1)Br)Br')
+mol_2=Chem.MolFromSmiles('c1cc(sc1Br)Br')
+
+# Creating a monomer with pysoftk
+a=Sm(mol_1,mol_2,"Br").monomer()
 
 # Print a smiles molecule to xyz format
-Fmt(mol_1).xyz_print("test_2.xyz")
+Fmt(a).xyz_print("test_2.xyz")
 
 # Print a smiles molecule to pdb format
-#Fmt(mol_1).pdb_print("test_2.pdb")
+Fmt(a).pdb_print("test_2.pdb")
 
 # Print a smiles molecule in mol format
-#Fmt(mol_1).mol_print("test_2.mol")
+Fmt(a).mol_print("test_2.mol")
 
 
 
