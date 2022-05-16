58066d91c9772b532c0cf61a7155c7a7557a1710
alejandrosantanabonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-05-16 11:37:59 +0100

    Update and rename test_htp.py~ to test_htp.py
    enabled the xtb relaxation option for HTP calculations

--- tests/result_test_htp/gfn_xtb/test_htp.py
+++ tests/result_test_htp/gfn_xtb/test_htp.py
@@ -1,10 +1,10 @@
 from calculator_htp import *
 
 # High-throughput calculations at the gfn-ff level of theory
-Htp("xyz").htp_xtb_ff("xtb",4,1)
+#Htp("xyz").htp_xtb_ff("xtb",4,1)
 
 # High-throughput calculation at the gnf-xtb level of theory
-#Htp("xyz").htp_xtb_gfn("xtb",2,2)
+Htp("xyz").htp_xtb_gfn("xtb",4,1)
 
 # High-throughput calculation at the PYSCF-semiempirial level of theory
 #Htp("xyz").htp_pyscf_semi(4, 10)
