ba05c4b27796ec446099f9935446f6274d5863c0
alejandrosantanabonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-05-18 13:25:25 +0100

    Update test_form_print.py

--- tests/test_form_print.py
+++ tests/test_form_print.py
@@ -7,19 +7,18 @@ from pysoftk.format_printers.format_mol import *
 # Molecules 1 and 2
 mol_1=Chem.MolFromSmiles('c1(ccc(cc1)Br)Br')
 mol_2=Chem.MolFromSmiles('c1cc(sc1Br)Br')
-<<<<<<< HEAD
 
 # Creating a monomer with pysiftk
 a=Sm(mol_1,mol_2,"Br").monomer()
 
 # Print a monomer to xyz format
-=======
+
 
 # Creating a monomer with pysoftk
 a=Sm(mol_1,mol_2,"Br").monomer()
 
 # Print a smiles molecule to xyz format
->>>>>>> 6a6dcd42bb13973dd0e41db3bd1664581fb5429e
+
 Fmt(a).xyz_print("test_2.xyz")
 
 # Print a smiles molecule to pdb format
