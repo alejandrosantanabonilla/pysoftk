2066576edc9bbc0b3004643cb3ffe03132bc5155
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-08-12 12:39:14 +0100

    Update test_single_monomers.py

--- test/test_single_monomers.py
+++ test/test_single_monomers.py
@@ -1,3 +1,4 @@
+import pytest
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import rdDistGeom
@@ -8,29 +9,87 @@ from pysoftk.linear_polymer.linear_polymer import *
 from pysoftk.format_printers.format_mol import *
 from pysoftk.folder_manager.folder_creator import *
 
-# SMILES all asymmetrical polymers
+# SMILES asymmetrical polymers testdata
+testdata=[
+    ('c1cc(sc1CBr)Br',1),
+    ('BrOCCBr',1),
+    ('[C@@H](CBr)(Br)C(=O)N',1),
+    ('C([C@@H](CN)Br)Br',1),
+    ('[C@@H](C(=O)OBr)(C)Br',1),
+]
 
-a=Chem.MolFromSmiles('c1cc(sc1CBr)Br')
-#a=Chem.MolFromSmiles('BrOCCBr')
-#a=Chem.MolFromSmiles('[C@@H](CBr)(Br)C(=O)N')
-#a=Chem.MolFromSmiles('C([C@@H](CN)Br)Br')
-#a=Chem.MolFromSmiles('C(/C=C(\C)/CBr)Br')
-#a=Chem.MolFromSmiles('[C@@H](C(=O)OBr)(C)Br')
-#a=Chem.MolFromSmiles('[C@@H](CBr)(Br)C(=O)OC')
-#a=Chem.MolFromSmiles('[C@](CBr)(Br)(C(=O)OC)C')
-#a=Chem.MolFromSmiles('c1c([C@@H](CBr)Br)cccc1')
+# SMILES asymmetrical polymers testdata_2
+testdata_2=[
+    ('[C@@H](C(=O)OBr)(C)Br',1),
+    ('[C@@H](CBr)(Br)C(=O)OC',1),
+    ('[C@](CBr)(Br)(C(=O)OC)C',1),
+    ('c1c([C@@H](CBr)Br)cccc1',1),
+]
 
-#Very Problematic
-#a=Chem.MolFromSmiles('[C@H](CBr)(OBr)C')
-#b=Chem.MolToSmiles(a, isomericSmiles=False)
-#c=Chem.MolFromSmiles(b)
+@pytest.mark.parametrize("mol,expected", testdata)
+def test_single_monomer(mol,expected):
+    
+    # Original Embedding
+    a=Chem.MolFromSmiles(str(mol))
+    AllChem.EmbedMolecule(a)
 
-# Original Embedding
-AllChem.EmbedMolecule(a)
+    new=Lp(a,"Br",7,shift=1.25).linear_polymer("MMFF",350)
+    Fmt(new).xyz_print("mol_1.xyz")
 
-# New Embedding
-#ps = rdDistGeom.ETKDGv3()
-#AllChem.EmbedMolecule(c,ps)
+    b=Fld().seek_files("xyz")
+    assert len(b) == expected
+    os.remove("mol_1.xyz")
 
-new=Lp(a,"Br",7,shift=1.25).linear_polymer("MMFF",350)
-Fmt(new).xyz_print("polymer_1_2.xyz")
+@pytest.mark.parametrize("mol,expected", testdata_2)
+def test_single_monomer_2(mol, expected):
+    
+    a=Chem.MolFromSmiles(str(mol))
+
+    #New_embedding
+    ps = rdDistGeom.ETKDGv3()
+    AllChem.EmbedMolecule(a,ps)
+
+    new=Lp(a,"Br",7,shift=1.25).linear_polymer("MMFF",350)
+    Fmt(new).xyz_print("mol_1.xyz")
+
+    b=Fld().seek_files("xyz")
+    
+    assert len(b) == expected
+    os.remove("mol_1.xyz")
+
+def test_problematic_1():
+    # Procedure for molecules with a very complicated stereochemistry
+    # One needs to read in SMILES, print into MOL and reload in SMILES
+    # MOL-format enables the creation of bonds and cures this issue.
+    
+    a=Chem.MolFromSmiles('[C@H](CBr)(OBr)C')
+    b=Chem.MolToSmiles(a, isomericSmiles=False)
+    c=Chem.MolFromSmiles(b)
+
+    ps = rdDistGeom.ETKDGv3()
+    AllChem.EmbedMolecule(c,ps)
+
+    new=Lp(c,"Br",7,shift=1.25).linear_polymer("MMFF",350)
+    Fmt(new).xyz_print("mol_1.xyz")
+
+    b=Fld().seek_files("xyz")
+    
+    assert len(b) == 1
+    os.remove("mol_1.xyz")
+    
+def test_problematic_2():
+    # Last example of a molecule withcomplicated definition.
+    
+    a=Chem.MolFromSmiles("C(/C=C(\C)/CBr)Br")
+
+    ps = rdDistGeom.ETKDGv3()
+    AllChem.EmbedMolecule(a,ps)
+
+    new=Lp(a,"Br",7,shift=1.25).linear_polymer("MMFF",350)
+    Fmt(new).xyz_print("mol_1.xyz")
+
+    b=Fld().seek_files("xyz")
+    
+    assert len(b) == 1
+    os.remove("mol_1.xyz")
+    
